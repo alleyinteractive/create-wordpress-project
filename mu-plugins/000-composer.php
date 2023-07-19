030495192931a9c9e13cd5ce9cdb0fc2ce65af87
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2023-07-19 11:45:18 -0400

    Add composer plugin installer, Pantheon mu-plugin, plugin loader

---
+++ mu-plugins/000-composer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Composer autoloader for WPR.
+ *
+ * This site depends on Composer autoloading to work, so this file needs to be first in the mu-plugins load sequence.
+ *
+ * @package wpr
+ */
+
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
