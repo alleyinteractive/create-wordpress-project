5dd1ba7394ba1b07e4b0710c9e195753ef2214e6
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2023-07-19 16:25:07 -0400

    Incorporate better DX into failed composer load

--- mu-plugins/000-composer.php
+++ mu-plugins/000-composer.php
@@ -1,10 +1,87 @@
 <?php
 /**
- * Composer autoloader for WPR.
+ * Composer Autoloader
  *
- * This site depends on Composer autoloading to work, so this file needs to be first in the mu-plugins load sequence.
+ * Load's Composer's autoloader if it exists and prompts the user to install it
+ * if it doesn't.
  *
- * @package wpr
+ * phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+ *
+ * @package create-wordpress-project
  */
 
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+$create_wordpress_project_autoloader = dirname( __DIR__ ) . '/vendor/autoload.php';
+
+// Define environment type, used in wp_get_environment_type below.
+switch ( $_ENV['PANTHEON_ENVIRONMENT'] ?? 'local' ) {
+	case 'develop':
+		define( 'WP_ENVIRONMENT_TYPE', 'development' );
+		break;
+	case 'live':
+		define( 'WP_ENVIRONMENT_TYPE', 'production' );
+		break;
+	case 'local':
+		define( 'WP_ENVIRONMENT_TYPE', 'local' );
+		break;
+	default:
+		define( 'WP_ENVIRONMENT_TYPE', 'staging' );
+		break;
+}
+
+// Display a friendly error message if Composer is not installed locally.
+if ( 'local' === wp_get_environment_type() ) {
+	if ( ! file_exists( $create_wordpress_project_autoloader ) ) {
+		wp_die(
+			'Composer is not installed. Please run <code>composer install</code> locally in the <code>wp-content</code> folder for this project.'
+		);
+	}
+
+	/** @var array{
+	 *   config?: array{
+	 *     platform?: array{
+	 *       php?: string,
+	 *     }
+	 *   }
+	 * } $composer Composer configuration from composer.json, which may specify the platform PHP version.
+	 */
+	$composer         = json_decode( file_get_contents( dirname( __DIR__ ) . '/composer.json' ) ?: '', true );
+	$required_version = $composer['config']['platform']['php'] ?? '8.0.0';
+
+	// Display a friendly error message if the wrong PHP version is used locally.
+	if ( version_compare( PHP_VERSION, $required_version, '<' ) ) {
+		wp_die(
+			sprintf(
+				'This site requires a PHP version >= %s. You are running %s. Please switch to <code>%s</code> on your machine.',
+				esc_html( $required_version ),
+				PHP_VERSION,
+				esc_html( $required_version ),
+			),
+		);
+	}
+
+	// Unset all variables to prevent any external usage.
+	unset( $composer, $required_version );
+}
+
+// Load the Composer autoloader or add a notice if it doesn't exist.
+if ( file_exists( $create_wordpress_project_autoloader ) ) {
+	require_once $create_wordpress_project_autoloader;
+} else {
+	// Include an error notice.
+	add_action(
+		'admin_notices',
+		function() {
+			if ( 'local' === wp_get_environment_type() ) {
+				printf(
+					'<div class="notice notice-error"><p>%s</p></div>',
+					esc_html( 'Composer needs to be installed for the site.' )
+				);
+			} else {
+				printf(
+					'<div class="notice notice-error"><p>%s</p></div>',
+					esc_html( 'Composer is not installed on the site! Please contact the site administrator.' )
+				);
+			}
+		}
+	);
+}
