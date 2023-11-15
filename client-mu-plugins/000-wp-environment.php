21e89f5c3d05baffaac76c2f7ed65d26b8ec6bfd
Sean Fisher <srtfisher@gmail.com>
2023-11-15 09:57:12 -0500

    Adding a readme that will be cleaned out by the configuration script

---
+++ client-mu-plugins/000-wp-environment.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Configuration for wp_get_environment_type().
+ *
+ * phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+ *
+ * @package example-project
+ */
+
+// Negotiate environment source based on Pantheon/WordPress VIP hosting environment.
+$environment_source = '';
+if ( defined( 'VIP_GO_APP_ENVIRONMENT' ) ) {
+	$environment_source = VIP_GO_APP_ENVIRONMENT;
+} elseif ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
+	$environment_source = $_ENV['PANTHEON_ENVIRONMENT'];
+}
+
+// Set the environment type to one of the wp_get_environment_type allowed values based on environment config.
+$environment_type = match ( $environment_source ) {
+	'dev', 'preprod', 'test' => 'staging',
+	'develop' => 'development',
+	'live', 'production' => 'production',
+	default => 'local',
+};
+define( 'WP_ENVIRONMENT_TYPE', $environment_type );
+
+// Don't pollute global scope.
+unset( $environment_source, $environment_type );
---
+++ client-mu-plugins/001-composer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Composer Autoloader
+ *
+ * Load's Composer's autoloader if it exists and prompts the user to install it
+ * if it doesn't.
+ *
+ * phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+ *
+ * @package example-project
+ */
+
+$composer_autoloader_path = dirname( __DIR__ ) . '/vendor/autoload.php';
+
+// Display a friendly error message if Composer is not installed locally.
+if ( 'local' === wp_get_environment_type() ) {
+	if ( ! file_exists( $composer_autoloader_path ) ) {
+		wp_die(
+			'Composer is not installed. Please run <code>composer install</code> locally in the <code>wp-content</code> folder for this project.'
+		);
+	}
+
+	/** @var array{
+	 *   config?: array{
+	 *     platform?: array{
+	 *       php?: string,
+	 *     }
+	 *   }
+	 * } $composer Composer configuration from composer.json, which may specify the platform PHP version.
+	 */
+	$composer         = json_decode( file_get_contents( dirname( __DIR__ ) . '/composer.json' ) ?: '', true );
+	$required_version = $composer['config']['platform']['php'] ?? '8.0.0';
+
+	// Display a friendly error message if the wrong PHP version is used locally.
+	if ( version_compare( PHP_VERSION, $required_version, '<' ) ) {
+		wp_die(
+			sprintf(
+				'This site requires a PHP version >= %s. You are running %s. Please switch to <code>%s</code> on your machine.',
+				esc_html( $required_version ),
+				PHP_VERSION,
+				esc_html( $required_version ),
+			),
+		);
+	}
+
+	// Unset all variables to prevent any external usage.
+	unset( $composer, $required_version );
+}
+
+// Load the Composer autoloader or add a notice if it doesn't exist.
+if ( file_exists( $composer_autoloader_path ) ) {
+	require_once $composer_autoloader_path;
+} else {
+	// Include an error notice.
+	add_action(
+		'admin_notices',
+		function() {
+			if ( 'local' === wp_get_environment_type() ) {
+				printf(
+					'<div class="notice notice-error"><p>%s</p></div>',
+					esc_html( 'Composer needs to be installed for the site.' )
+				);
+			} else {
+				printf(
+					'<div class="notice notice-error"><p>%s</p></div>',
+					esc_html( 'Composer is not installed on the site! Please contact the site administrator.' )
+				);
+			}
+		}
+	);
+}
+
+// Don't pollute global scope.
+unset( $composer_autoloader_path );
