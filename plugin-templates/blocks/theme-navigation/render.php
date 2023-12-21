f4aba47d8aac38699d680cffda037dc1af510fe5
efuller <eric@alley.co>
2023-12-21 15:30:01 -0500

    Add in theme navigation block template

---
+++ plugin-templates/blocks/theme-navigation/render.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The render callback for the create-wordpress-plugin/theme-navigation block.
+ *
+ * All of the parameters passed to the function where this file is being required are accessible in this scope:
+ *
+ * @var array<string, mixed> $attributes The array of attributes for this block.
+ * @var string               $content   Rendered block output. ie. <InnerBlocks.Content />.
+ * @var WP_Block             $block     The instance of the WP_Block class that represents the block being rendered.
+ *
+ * @package create-wordpress-plugin
+ */
+
+$create_wordpress_plugin_menu_location = isset( $attributes['menuLocation'] ) && is_string( $attributes['menuLocation'] ) ? $attributes['menuLocation'] : '';
+
+if ( empty( $create_wordpress_plugin_menu_location ) ) {
+	return;
+}
+
+?>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> data-location="<?php echo esc_attr( $create_wordpress_plugin_menu_location ); ?>">
+	<?php
+	wp_nav_menu(
+		[
+			'theme_location'  => $create_wordpress_plugin_menu_location,
+			'container_class' => 'menu-container',
+		]
+	);
+	?>
+</div>
