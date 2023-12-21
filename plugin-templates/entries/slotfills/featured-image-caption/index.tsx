635570091f596210708cd54dd680f4bb337e29d3
Greg Marshall <greg@alley.co>
2023-12-21 11:15:11 -0600

    code review feedback

--- plugin-templates/entries/slotfills/featured-image-caption/index.tsx
+++ plugin-templates/entries/slotfills/featured-image-caption/index.tsx
@@ -2,12 +2,12 @@
  * Adds a "Featured Image Caption" option to the featured image box.
  */
 import { addFilter } from '@wordpress/hooks';
+import JSXElement from 'react';
 import FeaturedImageCaption from './FeaturedImageCaption';
 
-// @ts-ignore
-function featuredImageCaption(OriginalComponent) {
-  // @ts-ignore
-  return function (props) { /* eslint-disable-line func-names */
+/* @ts-ignore - can't find the correct type for OriginalComponent */
+function featuredImageCaption(OriginalComponent: JSXElement) {
+  return function (props: object) { /* eslint-disable-line func-names */
     return (
       <>
         <OriginalComponent {...props} />
