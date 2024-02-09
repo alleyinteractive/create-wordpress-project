963d5ccb5deac6cc736d9c9b2548e3a1f6d9b710
Greg Marshall <greg@alley.co>
2024-02-09 14:59:27 -0600

    check plugin filesize

--- configure.php
+++ configure.php
@@ -674,7 +674,8 @@ $plugin_files = array_filter(
 		}
 
 		foreach ( $file_names as $file ) {
-			if ( file_exists( "plugins/{$plugin_dir}/{$file}" ) ) {
+			// Check if the file exists and is not (virtually) empty.
+			if ( file_exists( "plugins/{$plugin_dir}/{$file}" ) && 50 < filesize( "plugins/{$plugin_dir}/{$file}" ) {
 				return "'{$plugin_dir}/{$file}',";
 			}
 		}
