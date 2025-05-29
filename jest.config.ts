34ca962d0a8f3c75ecf2ec5dca3c7c03edbcd36f
Benjamin Bolton <7146063+benpbolton@users.noreply.github.com>
2025-05-29 11:55:18 -0600

    Update setupFilesAfterEnv...
    v5 and earlier: use @testing-library/jest-dom/extend-expect In @testing-library/jest-dom v6+: use just @testing-library/jest-dom  Since we're using v6.6.3, we should use the simpler import.

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,8 @@ const config: Config = {
     '@/(.*)': '<rootDir>/$1',
   },
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testMatch: [
     '**/plugins/create-wordpress-plugin/**/__tests__/**/*.ts?(x)',
     '**/themes/create-wordpress-theme/**/__tests__/**/*.ts?(x)',
