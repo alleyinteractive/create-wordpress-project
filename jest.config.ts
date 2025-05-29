db7c79f027dfc7d3c904ff3945f3a53fd3990717
Benjamin Bolton <7146063+benpbolton@users.noreply.github.com>
2025-05-29 11:58:38 -0600

    Support CSS module mocking with identity-obj-proxy

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
