1aec4cc704f0e709dacaf15e6559ee08684c3466
David Herrera <david@alley.com>
2026-09-21 01:49:27 -0400

    Fix a few linting and import mistakes

--- plugin-templates/blocks/theme-primary-term/render.php
+++ plugin-templates/blocks/theme-primary-term/render.php
@@ -2,6 +2,8 @@
 /**
  * The render callback for the create-wordpress-plugin/primary-term block.
  *
+ * @phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited -- File doesn't load in global scope, just appears to to PHPCS.
+ *
  * All of the parameters passed to the function where this file is being required are accessible in this scope:
  *
  * @phpstan-var array<string, mixed> $attributes
@@ -13,25 +15,36 @@
  * @package create-wordpress-plugin
  */
 
-$post_id           = $block->context['postId'] ?? get_the_ID();
-$post_id           = is_numeric( $post_id ) ? (int) $post_id : 0;
-$primary_term_rest = new \Create_WordPress_Plugin\Features\Primary_Term_Rest();
+
+if ( ! isset( $block->context['postId'] ) || ! is_numeric( $block->context['postId'] ) || ! $block->context['postId'] > 0 ) {
+	return;
+}
 
 if ( ! isset( $attributes['taxonomy'] ) || ! is_string( $attributes['taxonomy'] ) ) {
 	return;
 }
 
-$primary_term = $primary_term_rest->get_primary_term( $post_id, $attributes['taxonomy'] );
-if ( ! isset( $primary_term['term_link'], $primary_term['term_name'] ) ) {
+$primary_term = new \Create_WordPress_Plugin\Primary_Term( (int) $block->context['postId'], $attributes['taxonomy'] );
+$term         = get_term( $primary_term->term_id() );
+
+if ( ! $term instanceof WP_Term ) {
 	return;
 }
+
+$term_name = html_entity_decode( $term->name );
+$term_link = get_term_link( $term );
+
+if ( ! is_string( $term_link ) ) {
+	$term_link = '';
+}
+
 ?>
 <span <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<?php if ( $attributes['isLink'] ) : ?>
-		<a href="<?php echo esc_url( $primary_term['term_link'] ); ?>">
-			<?php echo esc_html( $primary_term['term_name'] ); ?>
+	<?php if ( $attributes['isLink'] && $term_link !== '' ) : ?>
+		<a href="<?php echo esc_url( $term_link ); ?>">
+			<?php echo esc_html( $term_name ); ?>
 		</a>
 	<?php else : ?>
-		<?php echo esc_html( $primary_term['term_name'] ); ?>
+		<?php echo esc_html( $term_name ); ?>
 	<?php endif; ?>
 </span>
